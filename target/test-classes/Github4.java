6841dbbdd2ce2a86a60d3b86aa371a79ab4e9477
Megha25M <meghahalli12@gmail.com>
2025-07-18 18:10:05 +0530

    commit

--- target/test-classes/Github4.java
+++ target/test-classes/Github4.java
@@ -3,6 +3,9 @@ public class Github4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("hii");
+		System.out.println("abcde");
+		System.out.println("12345");
 
 	}
 
